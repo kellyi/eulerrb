f0d7a7aa9345c9b8bbaf824edb5b7da2658732a6
Kelly Innes <kinnes@gmail.com>
2020-09-26 11:15:39 -0400

    Revert "Mistake"
    This reverts commit 9f98cb607095d0987b56dfa7820a822307431e02.

--- lib/eulerrb/utils.rb
+++ lib/eulerrb/utils.rb
@@ -5,7 +5,7 @@ require 'sorbet-runtime'
 module Utils
   extend T::Sig
 
-  sig { params(multiplicand: Numeric, multiplier: Numeric).returns(String) }
+  sig { params(multiplicand: Numeric, multiplier: Numeric).returns(Numeric) }
   def self.multiply(multiplicand:, multiplier:)
     multiplicand * multiplier
   end
