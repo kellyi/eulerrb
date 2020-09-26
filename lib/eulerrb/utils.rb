9f98cb607095d0987b56dfa7820a822307431e02
Kelly Innes <kinnes@gmail.com>
2020-09-26 11:12:18 -0400

    Mistake

--- lib/eulerrb/utils.rb
+++ lib/eulerrb/utils.rb
@@ -5,7 +5,7 @@ require 'sorbet-runtime'
 module Utils
   extend T::Sig
 
-  sig { params(multiplicand: Numeric, multiplier: Numeric).returns(Numeric) }
+  sig { params(multiplicand: Numeric, multiplier: Numeric).returns(String) }
   def self.multiply(multiplicand:, multiplier:)
     multiplicand * multiplier
   end
